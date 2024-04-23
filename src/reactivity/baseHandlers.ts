052e5e44cb262d7c09c7e82965a0174416cf0913
hhy0012 <hhyl.0012@gmail.com>
2024-04-23 13:26:24 +0800

    feat: effect-readonly

---
+++ src/reactivity/baseHandlers.ts
@@ -0,0 +1,36 @@
+import { track, trigger } from "./effect";
+
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key);
+
+    // TODO 依赖收集
+    if (!isReadonly) {
+      track(target, key);
+    }
+    return res;
+  }
+}
+
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+
+    // TODO 触发依赖
+    trigger(target, key);
+    return res;
+  }
+}
+
+export const mutableHandlers = {
+  get: createGetter(),
+  set: createSetter(),
+}
+
+export const readonlyHandlers = {
+  get: createGetter(false),
+  set(target, key, value) {
+    console.warn(`key:${key} cant set, target: ${target} is readonly!`)
+    return true; 
+  },
+}
