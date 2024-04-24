6fa132aa0cda376f448f59f87cc9e3e6c5ef6530
hhy0012 <hhyl.0012@gmail.com>
2024-04-24 08:25:25 +0800

    feat: ref-unRef

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,20 +1,21 @@
 import { effect } from "../effect";
-import { ref } from "../ref";
+import { reactive } from "../reactive";
+import { isRef, ref, unRef } from "../ref";
 
 describe("ref", () => {
-  it.skip("happy path", () => {
+  it("happy path", () => {
     const a = ref(1);
     expect(a.value).toBe(1);
   });
 
-  it.skip("should be reactive", () => {
+  it("should be reactive", () => {
     const a = ref(1);
     let dummy;
     let calls = 0;
     effect(() => {
       calls++;
       dummy = a.value;
-    })
+    });
     expect(calls).toBe(1);
     expect(dummy).toBe(1);
     a.value = 2;
@@ -32,9 +33,28 @@ describe("ref", () => {
     let dummy;
     effect(() => {
       dummy = a.value.count;
-    })
+    });
     expect(dummy).toBe(1);
     a.value.count = 2;
     expect(dummy).toBe(2);
   });
-})
+
+  it("isRef", () => {
+    const a = ref(1);
+    const user = reactive({
+      age: 1,
+    });
+    expect(isRef(a)).toBe(true);
+    expect(isRef(1)).toBe(false);
+    expect(isRef(user)).toBe(false);
+  });
+
+  it("unRef", () => {
+    const a = ref(1);
+    const user = reactive({
+      age: 1,
+    });
+    expect(unRef(a)).toBe(1);
+    expect(unRef(1)).toBe(1);
+  });
+});
